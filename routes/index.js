629cf4633ec2e5f0869e2d3bc787a1c315ab84e9
Zachary Smith <billyzacsmith@gmail.com>
2016-01-16 12:23:48 -0700

    Show an unordered list via: - A partial -- This takes an array and generates the list. - Call the partial on the main layout template.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,10 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Galvanize Reads' });
+  res.render('index', {
+    title: 'Galvanize Reads',
+    "musketeers": ["Athos", "Aramis", "Porthos", "D'Artagnan"]
+   });
 });
 
 module.exports = router;
