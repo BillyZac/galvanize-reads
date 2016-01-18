081d837a4aae530848e370783956fc52e01cb076
Zachary Smith <billyzacsmith@gmail.com>
2016-01-18 13:23:27 -0700

    Clean up author list view.

---
+++ public/js/handle-authors.js
@@ -0,0 +1,68 @@
+function handleDeleteAuthor() {
+  $('.delete').click(function() {
+    var authorId = $(this).data('id')
+    console.log('the id: ' + authorId)
+    $.ajax(getAPIHost() + '/authors/' + authorId, { method: 'delete' })
+    .then(function() {
+      window.location = '/authors'
+    })
+  })
+}
+
+function handleEditAuthor() {
+  $('.save-edit').click(function() {
+    var data = {
+      title: $('.title').val(),
+      genre: $('.genre').val(),
+      description: $('.description').val(),
+      cover_url: $('.cover-url').val()
+    }
+    if (formValid(data)) {
+        var authorId = $('.author-id').data('id')
+        console.log('This is the author id: ' + authorId)
+        // console.log('This is the author title: ' + $('.title').val())
+
+        // console.log('Saving author with this id: ' + authorId)
+      //   console.log('Saving author with this data: \n' +
+      //   'Title: ' + data.title + '\n' +
+      //   'Description: ' + data.description + '\n' +
+      //   'Genre: ' + data.genre + '\n' +
+      //   'cover_url: ' + data.cover_url
+      // )
+      $.ajax(getAPIHost() + '/authors/' + authorId, {
+        method: 'put',
+        data: data
+      })
+      .then(function() {
+        // window.location = '/authors'
+        window.location = '/authors/' + authorId
+      })
+    } else {
+      $('.message').text('Form cannot be blank.')
+    }
+  })
+}
+
+function formValid(data) {
+  for (item in data) {
+    console.log(data[item])
+    if (data[item] === '') {
+      return false
+    }
+  }
+  return true
+}
+
+function handleViewAuthor() {
+  $('.view-author').click(function() {
+    var authorId = $(this).data('id')
+    console.log('Clicked view author link. id: ' + authorId)
+    window.location = getAPIHost() + '/authors/' + authorId
+  })
+}
+
+$(function() {
+  handleDeleteAuthor()
+  handleEditAuthor()
+  handleViewAuthor()
+})
---
+++ routes/author.js
@@ -0,0 +1,92 @@
+var express = require('express');
+var router = express.Router();
+var knex = require('../db/knex')
+
+function Authors() {
+  return knex('authors')
+}
+
+
+
+router.get('/', function(req, res) {
+  Authors().select().then(function(authors) {
+    var length = authors.length
+    console.log('The authors array item 0: ', authors[0])
+    res.render('authors', {
+      title: "Galvanize Reads",
+      authors: authors
+    })
+  })
+
+})
+
+router.post('/', function(req, res) {
+  newBook = {
+    title: req.body.title,
+    genre: req.body.genre,
+    description: req.body.description,
+    cover_url: req.body.cover_url
+  }
+  Authors().insert(newBook, 'id').then(function(result) {
+    res.send('Created new author with id ' + result)
+  })
+})
+
+router.get('/:id', function(req, res) {
+    var id = req.params.id
+
+    Authors().select().where('id', id).then(function(results) {
+      var author = results[0]
+      if (author) {
+        res.render('author', {
+          title: "Galvanize Reads",
+          author: author
+        })
+      } else {
+        res.render('error', {
+          message: 'No such author.',
+          status: 404
+        })
+      }
+    })
+})
+
+router.put('/:id', function (req, res){
+  var data = req.body
+  var id = req.params.id
+
+  if (formValid(data)) {
+    Authors().where('id', id).update({
+      title: data.title,
+      genre: data.genre,
+      description: data.description,
+      cover_url: data.cover_url
+    }).then(function(result){
+      res.send('UPDATED item ' + id);
+    })
+  } else {
+    res.render('error', {
+        message: 'Form inputs cannot be blank.',
+        status: 400
+    })
+  }
+})
+
+function formValid(data) {
+  for (item in data) {
+    console.log(data[item])
+    if (data[item] === '') {
+      return false
+    }
+  }
+  return true
+}
+
+router.delete('/:id', function (req, res){
+  var id = req.params.id
+  Authors().where('id', id).del().then(function(result) {
+    res.send('DELETED. The result: ' + result)
+  })
+})
+
+module.exports = router;
