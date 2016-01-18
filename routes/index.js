ec31f83f872dc6e08b516eb2c208f8219a68e7d2
Zachary Smith <billyzacsmith@gmail.com>
2016-01-18 14:34:36 -0700

    Sidebar with nav to author and books page on authors page.

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,14 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function(req, res) {
+  var host = process.env.HOST || 'http://localhost:5000'
+  var links = {
+    authors: host + '/authors',
+    books:   host + '/books'
+  }
   res.render('index', {
-    title: 'Galvanize Reads'
+    title: 'Galvanize Reads',
+    links: links
    });
 });
 
