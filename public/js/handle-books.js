ef386fde4189143730dc9d9922c3364128e31cd5
Zachary Smith <billyzacsmith@gmail.com>
2016-01-18 11:37:32 -0700

    User can change the title of the book.

--- public/js/handle-books.js
+++ public/js/handle-books.js
@@ -18,9 +18,24 @@ function handleDeleteBook() {
 }
 
 function handleEditBook() {
-  console.log('Ready to save edits.')
   $('.save-edit').click(function() {
-    console.log('Saving the edit. Not really.')
+    var bookId = $('.book-id').data('id')
+    console.log('This is the book id: ' + bookId)
+    // console.log('This is the book title: ' + $('.title').val())
+    var data = {
+      title: $('.title').val()
+    }
+
+    // console.log('Saving book with this id: ' + bookId)
+    console.log('Saving book with this data.title: ' + data.title)
+    $.ajax(getAPIHost() + '/books/' + bookId, {
+      method: 'put',
+      data: data
+     })
+    .then(function() {
+      // window.location = '/books'
+      window.location = '/books/' + bookId
+    })
   })
 }
 
