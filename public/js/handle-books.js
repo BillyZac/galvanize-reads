ee77587ef409397834fa82fdda89e9ae2766e459
Zachary Smith <billyzacsmith@gmail.com>
2016-01-18 12:27:28 -0700

    Disallow blank fields in the form. Show user message if they try to submit a form with any blank fields.

--- public/js/handle-books.js
+++ public/js/handle-books.js
@@ -19,32 +19,46 @@ function handleDeleteBook() {
 
 function handleEditBook() {
   $('.save-edit').click(function() {
-    var bookId = $('.book-id').data('id')
-    console.log('This is the book id: ' + bookId)
-    // console.log('This is the book title: ' + $('.title').val())
     var data = {
       title: $('.title').val(),
       genre: $('.genre').val(),
       description: $('.description').val(),
       cover_url: $('.cover-url').val()
     }
+    if (formValid(data)) {
+        var bookId = $('.book-id').data('id')
+        console.log('This is the book id: ' + bookId)
+        // console.log('This is the book title: ' + $('.title').val())
 
-    // console.log('Saving book with this id: ' + bookId)
-    console.log('Saving book with this data: \n' +
-      'Title: ' + data.title + '\n' +
-      'Description: ' + data.description + '\n' +
-      'Genre: ' + data.genre + '\n' +
-      'cover_url: ' + data.cover_url
-    )
-    $.ajax(getAPIHost() + '/books/' + bookId, {
-      method: 'put',
-      data: data
-     })
-    .then(function() {
-      // window.location = '/books'
-      window.location = '/books/' + bookId
-    })
+        // console.log('Saving book with this id: ' + bookId)
+      //   console.log('Saving book with this data: \n' +
+      //   'Title: ' + data.title + '\n' +
+      //   'Description: ' + data.description + '\n' +
+      //   'Genre: ' + data.genre + '\n' +
+      //   'cover_url: ' + data.cover_url
+      // )
+      $.ajax(getAPIHost() + '/books/' + bookId, {
+        method: 'put',
+        data: data
+      })
+      .then(function() {
+        // window.location = '/books'
+        window.location = '/books/' + bookId
+      })
+    } else {
+      $('.message').text('Form cannot be blank.')
+    }
   })
+}
+
+function formValid(data) {
+  for (item in data) {
+    console.log(data[item])
+    if (data[item] === '') {
+      return false
+    }
+  }
+  return true
 }
 
 $(function() {
